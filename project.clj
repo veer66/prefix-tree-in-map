198658bfe74acca5f291a9d017df0b5a1891d971
vee <veer66@live.com>
2017-12-15 21:32:02 +0700

    move to github

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject prefix-tree-in-map "0.1.2"
   :description "prefix-tree-in-map is a library for storing and looking up a prefix tree in hash-map"
-  :url "https://gitlab.com/veer66/prefix-tree-in-map"
+  :url "https://github.com/veer66/prefix-tree-in-map.clj"
   :license {:name "LGPL v3"
             :url "http://www.gnu.org/licenses/lgpl.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
