b2f07f4315fc89ef6ca023d4453d170d652d619b
Vee Satayamas <vsatayamas@gmail.com>
2017-12-15 22:58:43 +0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject prefix-tree-in-map "0.1.3"
   :description "prefix-tree-in-map is a library for storing and looking up a prefix tree in hash-map"
-  :url "https://github.com/veer66/prefix-tree-in-map.clj"
+  :url "https://github.com/veer66/prefix-tree-in-map"
   :license {:name "Unlicense"
             :url "http://unlicense.org"}
   :dependencies [[org.clojure/clojure "1.9.0"]])
